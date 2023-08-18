f62d5fb973bafb6385dfe257a21f1acdbe8c96c0
nonefffds <fds@brunhild.cn>
2023-08-18 18:13:59 +0800

    更新计算分段得分部分

--- rhythm_operate.py
+++ rhythm_operate.py
@@ -165,31 +165,26 @@ class PlayEventNormal(_Event):
     # if event.user_data.self.user_data[2] / 15 - ref_min_rating > play_lev * 112:
     #
 """
-# 测试用，之后改
-def get_final_rating(random_rating: float):
-    final_rating : float
-    if 100 < random_rating < 100.5:
-        final_rating = 108 + (random_rating - 100) * 8
-    elif 99.5 < random_rating < 100:
-        final_rating = 105.5 + (random_rating - 99.5) * 5
-    elif 99 < random_rating < 99.5:
-        final_rating = 104 + (random_rating - 99) * 3
-    elif 98 < random_rating < 99:
-        final_rating = 101.5 + (random_rating - 98) * 5
-    elif 97 < random_rating < 98:
-        final_rating = 100 + (random_rating - 97) * 3
-    elif 94 < random_rating < 97:
-        final_rating = 84 + (random_rating - 94) * (5 + 1 / 3)
-    elif 90 < random_rating < 94:
-        final_rating = 68 + (random_rating - 90) * 4
-    elif 80 < random_rating < 90:
-        final_rating = 64 + (random_rating - 80) * 0.4
-    elif 0 < random_rating < 80:
-        final_rating = random_rating * 0.8
-    else: 
-        return
+# 计算分段得分
+def get_final_rating(random_rating: float) -> float:
+    rating_ranges = {
+        (100, 100.5): lambda x: 108 + (x - 100) * 8,
+        (99.5, 100): lambda x: 105.5 + (x - 99.5) * 5,
+        (99, 99.5): lambda x: 104 + (x - 99) * 3,
+        (98, 99): lambda x: 101.5 + (x - 98) * 5,
+        (97, 98): lambda x: 100 + (x - 97) * 3,
+        (94, 97): lambda x: 84 + (x - 94) * (5 + (1 / 3)),
+        (90, 94): lambda x: 68 + (x - 90) * 4,
+        (80, 90): lambda x: 64 + (x - 80) * 0.4,
+        (0, 80): lambda x: x * 0.8
+    }
 
-    return final_rating
+    for (lower, upper), formula in rating_ranges.items():
+        if lower < random_rating < upper:
+            return formula(random_rating)
+
+    return 0.0  # Default value if no range is matched
+
 class PlayEvent(_Event):
     """
     打歌事件
